2e7aaf7b52f6fd30bafa75f575e027785054edf0
Pola Malak <polamalak96@gmail.com>
2023-04-17 19:55:46 +0200

    refactor: add all modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BakersModule } from './bakers/bakers.module';
+import { DatabaseModule } from './database/database.module';
+import authConfig from './config/auth.config';
+import { PassportModule } from '@nestjs/passport';
+import { ProductsModule } from './products/products.module';
+import { SchedulesModule } from './schedules/schedules.module';
+import { RedisCacheModule } from './redis-cache/redis-cache.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig, authConfig],
+      isGlobal: true,
+    }),
+    PassportModule,
+    AuthModule,
+    DatabaseModule,
+    UsersModule,
+    BakersModule,
+    ProductsModule,
+    SchedulesModule,
+    RedisCacheModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
