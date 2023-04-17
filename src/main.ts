2ee80236435f54a619e5729fbd56a9c2e37fe3a8
Pola Malak <polamalak96@gmail.com>
2023-04-17 19:56:19 +0200

    feat: configure swagger open api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Cake-Pals API')
+    .setDescription('Cake-Pals API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .setExternalDoc('postman collection', 'v1-json')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
